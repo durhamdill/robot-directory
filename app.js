abf174f45b0abf279df10bcecd97cfc19c1d1598
Katy Dillard <durhamdill@gmail.com>
2017-08-16 12:17:59 -0400

    connect to data; build template

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mustacheExpress = require('mustache-express');
+const dataSource = require('./dataSource.js');
 
 const app = express();
 
@@ -8,8 +9,9 @@ app.engine('mustache', mustacheExpress());
 app.set('views', './views')
 app.set('view engine', 'mustache')
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/directory/', function (req, res) {
+  // res.send(dataSource);
+  res.render('directory', dataSource);
 });
 
 app.listen(3000, function () {
