9da275c346f874b3a6bfc59f48d9446818690b03
Katy Dillard <durhamdill@gmail.com>
2017-08-16 16:05:39 -0400

    add styles/content

--- app.js
+++ app.js
@@ -9,10 +9,17 @@ app.engine('mustache', mustacheExpress());
 app.set('views', './views')
 app.set('view engine', 'mustache')
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/directory/', function (req, res) {
-  // res.send(dataSource);
   res.render('directory', dataSource);
 });
+
+app.get('/directory/:id', function (req, res) {
+  let userid = req.params.id-1;
+  // let userindex= userid-1;
+  res.render('profile', dataSource.users[userid]);
+})
 
 app.listen(3000, function () {
   console.log('Successfully started express application!')
